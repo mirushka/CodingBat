744d5b4ff5190da4fcd5591e73a57be265456b52
Miroslava Gajdosova <mirka.gajdosova@gmail.com>
2019-03-16 10:23:03 +0000

    Initial Commit, Adding helloName method.

---
+++ src/com/company/String1.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class String1 {
+
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+
+
+    //Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+    public String helloName(String name) {
+
+        return "Hello " + name + "!";
+    }
+}
