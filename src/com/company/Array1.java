d8b8babb15fb323eb09ba18e91224cf303e5e622
Miroslava Gajdosova <mirka.gajdosova@gmail.com>
2019-03-16 11:50:30 +0000

    Adding sameFirstlast method

--- src/com/company/Array1.java
+++ src/com/company/Array1.java
@@ -15,4 +15,23 @@ public class Array1 {
 
     }
 
+    /* Array-1 > sameFirstLast
+    Given an array of ints, return true if the array is length 1 or more, and the first element and the last element are equal.
+    sameFirstLast([1, 2, 3]) → false
+    sameFirstLast([1, 2, 3, 1]) → true     */
+
+    public boolean sameFirstLast(int[] nums) {
+        if( nums.length>=1){
+            if(nums[0] == nums[nums.length-1]){
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }
+
+
+
 }
