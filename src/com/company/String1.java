449e11b6786c0d8c336c1c3918826ed51dc30332
Miroslava Gajdosova <mirka.gajdosova@gmail.com>
2019-03-16 10:31:01 +0000

    Adding makeAbba method, adding makeTags method

--- src/com/company/String1.java
+++ src/com/company/String1.java
@@ -8,11 +8,24 @@ public class String1 {
 
     }
 
-
-    //Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
-
+    // helloName
+    // Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
     public String helloName(String name) {
-
         return "Hello " + name + "!";
     }
+
+    // makeAbba
+    // Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+    public String makeAbba(String a, String b) {
+        return a+b+b+a;
+    }
+
+    // makeTags
+    // The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text. In this example, the "i" tag makes <i> and </i> which surround the word "Yay".
+    // Given tag and word strings, create the HTML string with tags around the word, e.g. "<i>Yay</i>".
+    public String makeTags(String tag, String word) {
+        return "<"+ tag +">" + word + "</"+ tag +">";
+
+    }
+
 }
