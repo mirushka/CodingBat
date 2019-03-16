ec21dc06df774aa90fecc6a4fb0ef0dea956843b
Miroslava Gajdosova <mirka.gajdosova@gmail.com>
2019-03-16 15:32:20 +0000

    Adding comboString method

--- src/com/company/String1.java
+++ src/com/company/String1.java
@@ -67,4 +67,16 @@ public class String1 {
         return modif;
     }
 
+    /* String-1 > comboString
+    Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty (length 0).
+    comboString("Hello", "hi") → "hiHellohi"
+    comboString("hi", "Hello") → "hiHellohi"     */
+    public String comboString(String a, String b) {
+
+        if (a.length() > b.length()) {
+            return b + a + b;
+        } else {
+            return a + b + a;
+        }
+    }
 }
