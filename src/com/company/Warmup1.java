fcf581a5925ebfc8d995efff8b51ed0de2804d07
Miroslava Gajdosova <mirka.gajdosova@gmail.com>
2019-03-17 15:51:04 +0000

    Adding parrotTrouble method

--- src/com/company/Warmup1.java
+++ src/com/company/Warmup1.java
@@ -63,8 +63,25 @@ public class Warmup1 {
         return 2 * result * (-1);
     }
 
-    /*
+    /* Warmup-1 > parrotTrouble
+    We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20.
+    Return true if we are in trouble.
+    parrotTrouble(true, 6) → true
+    parrotTrouble(true, 7) → false       */
+    public boolean parrotTrouble(boolean talking, int hour) {
+        if ((hour < 7 & talking) || (hour > 20 & talking)) {
+            return true;
+        }
+        return false;
+    }
+    /* Solution 2
+    public boolean parrotTrouble(boolean talking, int hour) {
+  if(talking && (hour<7 || hour>20)){
+    return true;
+  }
+  return false; }
+    */
 
-     */
+
 
 }
