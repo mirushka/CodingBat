e206817b6a83ac5630278cef7fd01e997ccbb3ee
Miroslava Gajdosova <mirka.gajdosova@gmail.com>
2019-03-24 14:34:33 +0000

    Adding no23 method.

--- src/com/company/Array1.java
+++ src/com/company/Array1.java
@@ -139,6 +139,21 @@ public class Array1 {
         return false;
     }
 
+    /* Array-1 > no23
+    Given an int array length 2, return true if it does not contain a 2 or 3.
+    no23([4, 5]) → true
+     */
+    public boolean no23(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 2 || nums[i] == 3) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+
 
 
 
