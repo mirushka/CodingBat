8d22f53ff773b619699d4b6a5f56c86296e5af8e
Miroslava Gajdosova <mirka.gajdosova@gmail.com>
2019-03-27 10:06:25 +0000

    Creating Map1 class, adding  mapBully  method to Map-1.

---
+++ src/com/company/Map1.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Map1 {
+    /* Map-1 > mapBully
+    Modify and return the given map as follows: if the key "a" has a value, set the key "b" to have that value, and set the key "a" to have the value "". Basically "b" is a bully, taking the value and replacing it with the empty string.
+    mapBully({"a": "candy", "b": "dirt"}) → {"a": "", "b": "candy"}
+    mapBully({"a": "candy"}) → {"a": "", "b": "candy"}
+     */
+    public Map<String, String> mapBully(Map<String, String> map) {
+        if (map.containsKey("a")) {
+            map.put("b", map.get("a"));
+            map.put("a", "");
+        }
+        return map;
+    }
+
+
+}
