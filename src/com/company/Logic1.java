e58fe16b1b1db2daf9a187d129e0a56a4d67ed7c
Miroslava Gajdosova <mirka.gajdosova@gmail.com>
2019-03-27 08:40:58 +0000

    Adding  specialEleven method to Logic-1.

--- src/com/company/Logic1.java
+++ src/com/company/Logic1.java
@@ -151,10 +151,15 @@ public class Logic1 {
     public boolean in1To10(int n, boolean outsideMode) {
   if(outsideMode)
     return (n <= 1 || n>=10);
-  return(1<=n && n<=10);
-}
+  return(1<=n && n<=10);}     */
 
+    /* Logic-1 > specialEleven
+    We'll say a number is special if it is a multiple of 11 or if it is one more than a multiple of 11. Return true if the given non-negative number is special. Use the % "mod" operator -- see Introduction to Mod
+    specialEleven(22) → true
+    specialEleven(23) → true
      */
-
+    public boolean specialEleven(int n) {
+        return (n % 11 == 0 || n % 11 == 1);
+    }
 
 }
