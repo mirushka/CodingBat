e361cc3ff17da6972e1c93ff8d01d32e062f6e24
Miroslava Gajdosova <mirka.gajdosova@gmail.com>
2019-03-27 10:31:13 +0000

    Adding  mapShare  method to Map-1.

--- src/com/company/Map1.java
+++ src/com/company/Map1.java
@@ -14,5 +14,19 @@ public class Map1 {
         return map;
     }
 
+    /*  Map-1 > mapShare
+    Modify and return the given map as follows: if the key "a" has a value, set the key "b" to have that same value. In all cases remove the key "c", leaving the rest of the map unchanged.
+    mapShare({"a": "aaa", "b": "bbb", "c": "ccc"}) → {"a": "aaa", "b": "aaa"}
+     */
+    public Map<String, String> mapShare(Map<String, String> map) {
+        if (map.containsKey("a")) {
+            map.put("b", map.get("a"));
+            map.remove("c");
+        }
+        map.remove("c");
+        return map;
+
+    }
+
 
 }
