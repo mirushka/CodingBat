e6ce3df98d0dd3a3d8cd8f823ef9c4984b5ff9c4
Miroslava Gajdosova <mirka.gajdosova@gmail.com>
2019-03-29 06:40:34 +0000

    Adding more20 method to Logic-1.

--- src/com/company/Logic1.java
+++ src/com/company/Logic1.java
@@ -162,4 +162,13 @@ public class Logic1 {
         return (n % 11 == 0 || n % 11 == 1);
     }
 
+    /*  Logic-1 > more20
+    Return true if the given non-negative number is 1 or 2 more than a multiple of 20. See also: Introduction to Mod
+    more20(20) → false
+     */
+    public boolean more20(int n) {
+        return (n % 20 == 1 || n % 20 == 2);
+    }
+
+
 }
