c47400db73a436a5d435062e0535538321c79c4c
Miroslava Gajdosova <mirka.gajdosova@gmail.com>
2019-03-29 17:56:00 +0000

    Adding old35 method to Logic-1.

--- src/com/company/Logic1.java
+++ src/com/company/Logic1.java
@@ -170,5 +170,14 @@ public class Logic1 {
         return (n % 20 == 1 || n % 20 == 2);
     }
 
+    /*  Logic-1 > old35
+    Return true if the given non-negative number is a multiple of 3 or 5, but not both. Use the % "mod" operator.
+    old35(3) → true
+     */
+    public boolean old35(int n) {
+        return (n % 3 == 0) != (n % 5 == 0);
+    }
+
+
 
 }
