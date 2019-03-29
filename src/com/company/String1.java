303fc5983f488ddd0fa36d42ae3b1da504938333
Miroslava Gajdosova <mirka.gajdosova@gmail.com>
2019-03-29 17:51:46 +0000

    Adding extraFront method to String-1.

--- src/com/company/String1.java
+++ src/com/company/String1.java
@@ -245,5 +245,18 @@ public class String1 {
         return "";
     }
 
+    /* String-1 > extraFront
+    Given a string, return a new string made of 3 copies of the first 2 chars of the original string. The string may be any length. If there are fewer than 2 chars, use whatever is there.
+    extraFront("Hello") → "HeHeHe"
+     */
+    public String extraFront(String str) {
+        if (str.length() >= 2) {
+            str = str.substring(0, 2);
+            return str + str + str;
+        }
+        return str + str + str;
+    }
+
+
 
 }
