08cff63ea587a712ee67191f6993e66805dc9cc5
Miroslava Gajdosova <mirka.gajdosova@gmail.com>
2019-04-09 19:53:52 +0100

    Adding frontBack method to Warmup1.

--- src/com/company/Warmup1.java
+++ src/com/company/Warmup1.java
@@ -141,5 +141,21 @@ public class Warmup1 {
         return buff.deleteCharAt(n).toString();
     }
 
+    /* Warmup-1 > frontBack
+    Given a string, return a new string where the first and last chars have been exchanged.
+    frontBack("code") → "eodc"
+    frontBack("a") → "a"
+     */
+    public String frontBack(String str) {
+        if (str.length() <= 1) {
+            return str;
+        }
+
+        char first = str.charAt(0);
+        char last = str.charAt(str.length() - 1);
+        return last + str.substring(1, str.length() - 1) + first;
+
+    }
+
 
 }
