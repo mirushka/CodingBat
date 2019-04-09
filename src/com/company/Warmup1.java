ddc7761ab13bd5dc97d6667f0cb9fecb71bd1c49
Miroslava Gajdosova <mirka.gajdosova@gmail.com>
2019-04-09 19:42:45 +0100

    Adding missingChar method to Warmup1.

--- src/com/company/Warmup1.java
+++ src/com/company/Warmup1.java
@@ -129,5 +129,17 @@ public class Warmup1 {
         return "not " + str;
     }
 
+   /* Warmup-1 > missingChar
+    Given a non-empty string and an int n, return a new string where the char at index n has been removed.
+    The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..str.length()-1 inclusive).
+    missingChar("kitten", 1) → "ktten"
+    missingChar("kitten", 0) → "itten" */
+
+    public String missingChar(String str, int n) {
+
+        StringBuffer buff = new StringBuffer(str);
+        return buff.deleteCharAt(n).toString();
+    }
+
 
 }
