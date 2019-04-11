f677eb36ac2916b2abab5b1c95e4725a4cc85b33
Miroslava Gajdosova <mirka.gajdosova@gmail.com>
2019-04-11 19:31:57 +0100

    Adding nearTen method to Logic1.

--- src/com/company/Logic1.java
+++ src/com/company/Logic1.java
@@ -190,6 +190,16 @@ public class Logic1 {
         return false;
     }
 
+    /* Logic-1 > nearTen
+    Given a non-negative number "num", return true if num is within 2 of a multiple of 10. Note: (a % b) is the remainder of dividing a by b, so (7 % 5) is 2. See also: Introduction to Mod
+    nearTen(12) → true
+    nearTen(17) → false
+     */
+    public boolean nearTen(int num) {
+        return num % 10 <= 2 || num % 10 >= 8;
+    }
+
+
 
 
 }
