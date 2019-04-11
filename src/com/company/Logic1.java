45e097ea670721e37f1c25c399070c0b6323c516
Miroslava Gajdosova <mirka.gajdosova@gmail.com>
2019-04-11 19:24:36 +0100

    Adding less20 method to Logic1.

--- src/com/company/Logic1.java
+++ src/com/company/Logic1.java
@@ -178,6 +178,18 @@ public class Logic1 {
         return (n % 3 == 0) != (n % 5 == 0);
     }
 
+    /* Logic-1 > less20
+    Return true if the given non-negative number is 1 or 2 less than a multiple of 20. So for example 38 and 39 return true, but 40 returns false. See also: Introduction to Mod
+    less20(18) → true
+    less20(19) → true
+     */
+    public boolean less20(int n) {
+        if (n % 20 == 18 || n % 20 == 19) {
+            return true;
+        }
+        return false;
+    }
+
 
 
 }
