0520bed3ae2d86f3d2a79c2d3d3205e06e024a5d
Miroslava Gajdosova <mirka.gajdosova@gmail.com>
2019-04-12 20:18:21 +0100

    Adding front22 method to Logic1.

--- src/com/company/Logic1.java
+++ src/com/company/Logic1.java
@@ -229,6 +229,22 @@ public class Logic1 {
         return true;
     }
 
+    /* Logic-1 > fizzString
+    Given a string str, if the string starts with "f" return "Fizz".
+    If the string ends with "b" return "Buzz". If both the "f" and "b" conditions are true, return "FizzBuzz". In all other cases, return the string unchanged. (See also: FizzBuzz Code)
+    fizzString("fig") → "Fizz"
+     */
+    public String fizzString(String str) {
+        if (str.startsWith("f") && str.endsWith("b")) {
+            return "FizzBuzz";
+        } else if (str.startsWith("f")) {
+            return "Fizz";
+        } else if (str.endsWith("b")) {
+            return "Buzz";
+        }
+        return str;
+    }
+
 
 }
 
