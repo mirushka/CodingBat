efe963b7c36b917dd78b13fca091f760168a7117
Miroslava Gajdosova <mirka.gajdosova@gmail.com>
2019-04-12 06:42:45 +0100

    Adding teenSum  method to Logic1.

--- src/com/company/Logic1.java
+++ src/com/company/Logic1.java
@@ -200,6 +200,18 @@ public class Logic1 {
     }
 
 
+    /* Logic-1 > teenSum
+    Given 2 ints, a and b, return their sum. However, "teen" values in the range 13..19 inclusive, are extra lucky. So if either value is a teen, just return 19.
+    teenSum(3, 4) → 7
+    teenSum(10, 13) → 19
+     */
+    public int teenSum(int a, int b) {
+        int sum = a + b;
+        if (a >= 13 && a <= 19 || b >= 13 && b <= 19) {
+            return sum = 19;
+        }
+        return sum;
+    }
 
 
 }
