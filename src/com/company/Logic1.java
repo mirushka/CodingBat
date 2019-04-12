8c7a89e847d0acb36a9149fa9900b43bcadd8c2c
Miroslava Gajdosova <mirka.gajdosova@gmail.com>
2019-04-12 20:37:28 +0100

    Adding inOrder method to Logic1.

--- src/com/company/Logic1.java
+++ src/com/company/Logic1.java
@@ -254,6 +254,21 @@ public class Logic1 {
         return (a + b == c || b + c == a || c + a == b);
     }
 
+    /* Logic-1 > inOrder
+    Given three ints, a b c, return true if b is greater than a, and c is greater than b. However, with the exception that if "bOk" is true, b does not need to be greater than a.
+    inOrder(1, 2, 4, false) → true
+    inOrder(1, 2, 1, false) → false
+     */
+    public boolean inOrder(int a, int b, int c, boolean bOk) {
+        if (bOk && c > b) {
+            return true;
+        }
+        if (b > a && c > b) {
+            return true;
+        }
+        return false;
+    }
+
 
 
 }
