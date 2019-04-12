7cfa9657886f7c48e1005d886f4c0a7922dddc31
Miroslava Gajdosova <mirka.gajdosova@gmail.com>
2019-04-12 06:50:56 +0100

    Adding answerCell   method to Logic1.

--- src/com/company/Logic1.java
+++ src/com/company/Logic1.java
@@ -213,5 +213,25 @@ public class Logic1 {
         return sum;
     }
 
+    /* Logic-1 > answerCell
+    Your cell phone rings. Return true if you should answer it.
+    Normally you answer, except in the morning you only answer if it is your mom calling.
+    In all cases, if you are asleep, you do not answer.
+    answerCell(false, false, false) → true
+     */
+    public boolean answerCell(boolean isMorning, boolean isMom, boolean isAsleep) {
+        if (isAsleep) {
+            return false;
+        }
+        if (isMorning && !isMom) {
+            return false;
+        }
+        return true;
+    }
+
 
 }
+
+
+
+
