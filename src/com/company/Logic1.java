b58088c6f03a9808292da98cc3ad4e847a68e04e
Miroslava Gajdosova <mirka.gajdosova@gmail.com>
2019-04-14 07:08:13 +0100

    Adding inOrderEqual method to Logic1.

--- src/com/company/Logic1.java
+++ src/com/company/Logic1.java
@@ -269,6 +269,21 @@ public class Logic1 {
         return false;
     }
 
+    /* Logic-1 > inOrderEqual
+    Given three ints, a b c, return true if they are in strict increasing order, such as 2 5 11, or 5 6 7, but not 6 5 7 or 5 5 7. However, with the exception that if "equalOk" is true, equality is allowed, such as 5 5 7 or 5 5 5.
+    inOrderEqual(2, 5, 11, false) → true
+    inOrderEqual(5, 7, 6, false) → false
+     */
+    public boolean inOrderEqual(int a, int b, int c, boolean equalOk) {
+        if (equalOk && a <= b && b <= c) {
+            return true;
+        }
+        if (a < b && b < c) {
+            return true;
+        }
+        return false;
+    }
+
 
 
 }
